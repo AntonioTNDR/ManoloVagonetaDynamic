11d2b7e3f8128ebb0d8696b27762a80b58c4d8c6
JorgeGarciaMarin <127936581+JorgeGarciaMarin@users.noreply.github.com>
2024-04-14 13:46:54 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 
 
-int dynamical(int rows,int columns,int matrix[rows][columns], int elements[],int selected[]){ //El array selected para guardar los pesos de los elementos seleccionados
+int dynamical(int rows,int columns,int matrix[rows][columns], int elements[],int selected[],int * nElements){ //El array selected para guardar los pesos de los elementos seleccionados
     int totalWeight = 0; //Para ir sumando los pesos cuando seleccionemos los items
 //We initialize the first row
     for (int j=0;j<columns;j++){
@@ -22,14 +22,14 @@ int dynamical(int rows,int columns,int matrix[rows][columns], int elements[],int
         }
     }
     //Aqui todo el proceso de seleccionar
+
     int n=0;
     int j=columns-1;
+    *nElements=0;
    for(int i=rows;i>0;i--){
       if(matrix[i][j]!=matrix[i-1][j]){
         selected[n]=elements[i-1];
-          printf("%d,%d\n",matrix[i][j],matrix[i-1][j]);
-          printf("%d\n",i);
-          printf("Elements[i-1]=%d\n",selected[n]);
+        *nElements=*nElements+1;
         totalWeight+=elements[i-1];
         j=j-elements[i-1];
         n++;
@@ -56,6 +56,7 @@ int main() {
     int selected[100];
     int value;
     int i=0;
+    int  nElements=0;
     while(1){
         scanf("%d",&value);
         if(value==-1) break;
@@ -66,11 +67,17 @@ int main() {
     int matrix[i+1][15];
     int rows=i+1;
     int columns=15;
-    int price=dynamical(rows,columns,matrix,list,selected);
+    int price=dynamical(rows,columns,matrix,list,selected,&nElements);
     for (int j=0;j<rows;j++){
         for (int k=0;k<columns;k++){
             printf("%d\t",matrix[j][k]);
         }
+    }
+    printf("Selected elements:\n");
+    for (int x=0;x<nElements;x++){
+        if(x==nElements-1){printf("%d",selected[x]);}
+        else printf("%d,",selected[x]);
+
     }
 
     printf("\n%d",price);
