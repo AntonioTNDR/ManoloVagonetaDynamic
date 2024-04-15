f3b8041b7136baa010d04563057036df00784253
ByDageta <119523917+ByDageta@users.noreply.github.com>
2024-04-15 10:47:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,84 +1,82 @@
 #include <stdio.h>
 
+const int CAPACITY = 1500; // Maximum lorry capacity
 
-
-
-
-
-int dynamical(int rows,int columns,int matrix[rows][columns], int elements[],int selected[],int * nElements){ //El array selected para guardar los pesos de los elementos seleccionados
-    int totalWeight = 0; //Para ir sumando los pesos cuando seleccionemos los items
-//We initialize the first row
-    for (int j=0;j<columns;j++){
-        matrix[0][j]=0;
+// DYNAMICAL PROGRAMMING ALGORITHM]
+// selected[] = Array containing the selected items
+// nElements = # of elements
+int dynamical(int rows, int elements[], int selected[], int* nElements) {
+    // Compute minimum number of columns for the matrix
+    int columns = 0;
+    for (int i = 0; i < rows; ++i) {
+        columns += elements[i];
     }
-//Crear tabla
-    for (int i=1;i<=rows;i++) {
+    columns++; // One more column (to consider 0 total capacity)
+    if (columns > CAPACITY) columns = CAPACITY + 1; // Don't accept any more than 15k columns (max lorry capacity)
+
+    rows++; // One more row (to consider 0 items)
+    int totalWeight = 0;
+    int matrix[rows][columns];
+
+    // Initialize the 1st row to 0
+    for (int j = 0; j < columns; j++) {
+        matrix[0][j] = 0;
+    }
+
+    // Get optimal solution for each size and number of elements
+    // Traverse the matrix computing the optimal states
+    for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
-            if (elements[i - 1] > j) { matrix[i][j] = matrix[i - 1][j]; }
+            if (elements[i - 1] > j) matrix[i][j] = matrix[i - 1][j];
             else {
-                if (matrix[i-1][j]>(matrix[i-1][j-elements[i-1]]+elements[i-1])) matrix[i][j]=matrix[i-1][j];
-                else{matrix[i][j]=matrix[i-1][j-elements[i-1]]+elements[i-1];}
+                if (matrix[i-1][j] > (matrix[i-1][j-elements[i-1]] + elements[i-1])) matrix[i][j] = matrix[i-1][j];
+                else matrix[i][j] = matrix[i-1][j-elements[i-1]] + elements[i-1];
             }
         }
     }
-    //Aqui todo el proceso de seleccionar
+    // printf("columns = %d\n", columns); // Just to check if it's working
+    // printf("rows = %d\n", rows);
 
-    int n=0;
-    int j=columns-1;
-    *nElements=0;
-   for(int i=rows;i>0;i--){
-      if(matrix[i][j]!=matrix[i-1][j]){
-        selected[n]=elements[i-1];
-        *nElements=*nElements+1;
-        totalWeight+=elements[i-1];
-        j=j-elements[i-1];
-        n++;
-      }
-   }
+    // Obtain the selected items
+    int n = 0;
+    int j = columns - 1;
+    *nElements = 0; // Initialize to 0 elements included
 
-
-    return totalWeight*6; //precio en euros
+    for(int i = rows - 1; i > 0; i--) {
+        if(matrix[i][j] != matrix[i-1][j]) { // If the optimal weight changed this iteration
+            selected[n] = elements[i-1]; // Include this element
+            n++;
+            *nElements = *nElements + 1; // Count 1 more element
+            totalWeight += elements[i-1]; // Count the weight
+            j = j - elements[i-1]; // Move the current pointer weight(i*) to the left to continue checking
+        }
+    }
+    return totalWeight * 6; // Return price
 }
-
-
-
-
-
-
-
-
-
-
 
 
 int main() {
     int list[20];
     int selected[100];
+    int nElements = 0;
     int value;
-    int i=0;
-    int  nElements=0;
+    int i = 0;
+
     while(1){
-        scanf("%d",&value);
-        if(value==-1) break;
-        list[i]=value;
+        scanf("%d", &value);
+        if(value == -1) break;
+        list[i] = value;
         i++;
-
     }
-    int matrix[i+1][15];
-    int rows=i+1;
-    int columns=15;
-    int price=dynamical(rows,columns,matrix,list,selected,&nElements);
-    for (int j=0;j<rows;j++){
-        for (int k=0;k<columns;k++){
-            printf("%d\t",matrix[j][k]);
-        }
-    }
-    printf("Selected elements:\n");
-    for (int x=0;x<nElements;x++){
-        if(x==nElements-1){printf("%d",selected[x]);}
-        else printf("%d,",selected[x]);
+    int rows = i; // rows = i = # of elements introduced
+    int price = dynamical(rows, list, selected, &nElements);
 
+    printf("\nSelected elements:\n");
+    for (int x = 0; x < nElements; x++){
+        if(x == nElements - 1) printf("%d", selected[x]);
+        else printf("%d, ", selected[x]);
     }
+    printf("\nTotal price = %d\n", price);
 
-    printf("\n%d",price);
+    while(1); // Endless loop to prevent executable program immediately closing after finishing
 }
